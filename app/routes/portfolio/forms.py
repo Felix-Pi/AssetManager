af6c651dc7b01f10fc11dac665e745db4a2eeb3e
Felix-Pi <49276377+Felix-Pi@users.noreply.github.com>
2021-05-17 11:19:21 +0200

    added add_transaction form

--- app/routes/portfolio/forms.py
+++ app/routes/portfolio/forms.py
@@ -3,28 +3,23 @@ from datetime import datetime
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField, FloatField, SelectField
 from wtforms.fields.html5 import EmailField, IntegerField, DecimalField, DateField
-from wtforms.validators import ValidationError, DataRequired, EqualTo, Email
+from wtforms.validators import ValidationError, DataRequired, EqualTo, Email, InputRequired
 
-
-# class MyFloatField(DecimalField):
-#     def process_formdata(self, valuelist):
-#         if valuelist:
-#             try:
-#                 self.data = float(valuelist[0].replace(',', '.'))
-#             except ValueError:
-#                 self.data = None
-#                 raise ValueError(self.gettext('Not a valid float value'))
-from app import db, Asset_types
+from app import db, Asset_types, Transaction_types, Portfolio, app
 
 
 class AddTransactionForm(FlaskForm):
-    today = datetime.now() #.strftime('%d.%m.%Y')
-    type = SelectField('Type', validators=[DataRequired()], choices=[(a.id, a.type) for a in db.session.query(Asset_types).all()])
+    today = datetime.now()  # .strftime('%d.%m.%Y')
 
+    portfolio = SelectField('Portfolio', validators=[DataRequired()],
+                            choices=[])
 
-    symbol = StringField('Symbol', validators=[DataRequired()])
+    transaction_type = SelectField('Transaction Type', validators=[DataRequired()],
+                                   choices=[(a.id, a.title) for a in db.session.query(Transaction_types).all()])
+
+    symbol = StringField('Symbol', validators=[])
     price = DecimalField('Price', validators=[DataRequired()])
     quantity = DecimalField('Quantity', validators=[DataRequired()])
-    fee = DecimalField('Fee', validators=[DataRequired()])
-    date = DateField('Date', validators=[DataRequired()], default=today)
+    fee = DecimalField('Fee', validators=[InputRequired()])
+    date = DateField('Date', validators=[DataRequired()], default=today, format='%d.%m.%y')
     submit = SubmitField('Add transaction')
